0cbf7b7bc35020637c0f7643395184371fe15b19
aadesh0706 <aadeshgulumbe3@gmail.com>
2023-11-10 02:42:49 -0800

    student_attendance

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAvwArJtiB-QhxIsfDZpnPhxGt6gPSr7us",
+    authDomain: "attendancesystem-32ab2.firebaseapp.com",
+    databaseURL: "https://attendancesystem-32ab2-default-rtdb.firebaseio.com/",
+    projectId: "attendancesystem-32ab2",
+    storageBucket: "attendancesystem-32ab2.appspot.com",
+    messagingSenderId: "164441153104",
+    appId: "1:164441153104:web:aeaf208e310cb3d6bcfb55",
+    measurementId: "G-LSCTJWZ5PJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
